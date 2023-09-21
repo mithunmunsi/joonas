b97ab56370ee122255e323cccd90d7696c65bb75
Mithun Munsi <mithun.munsi@gmail.com>
2023-09-22 00:27:04 +0300

    two projects added

---
+++ Arrays/main.js
@@ -0,0 +1,48 @@
+const prog = ['Web', 'REACT', 'DEV', 'CYBER'];
+prog.splice(2, 0, 'PELI', 'CLOUD');
+console.log(prog);
+// Task 1
+// Create an array called "names" and set it's items to be
+// David, Ann, Leia, Mathew and Mark.
+// Print the array.
+const names = ['David', 'Ann', 'Lwia', 'Mathew', 'Mark'];
+
+const scores = [23, 34, 45, 56, 67, 78, 89];
+document.getElementById('task1').textContent = scores.join(' ');
+
+// Task 2
+// Print the second item of the array from the task 1.
+// Loop Over an array
+const birds = ['Parrot', 'Falcon', 'Owl'];
+for (const bird of birds) {
+  console.log(bird);
+}
+// Task 3
+// Print the length of the array.
+console.log(names.length);
+
+// Task 4
+// Print the last item of the array
+
+// Task 5
+// Add Peter as the last item to the array and print the whole array
+
+// Task 6
+// Print the array with spaces, not commas
+names.join('');
+// Task 7
+// Replace Mathew in the array with Lisa and Abraham
+
+// Task 8
+// Sort the array in the alphabetical order and print it
+
+// Task 9
+// Create an array called numbers and fill it with values 1, 7, 45, 32, 27, 86.
+// Sort the numbers from largest to smallest. Print them.
+
+// Task 10
+// Randomize the numbers array.
+
+// Task 11
+// Find the largest and smallest value from the numbers array using sort() function.
+// Print those values.
